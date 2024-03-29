27964ab8c67738aaa53dfbe09c5ad6ab0fd3e546
gabrieliit <gabrielthomas.v@gmail.com>
2024-03-29 09:06:20 +0530

    Hello World fask app

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,6 @@ from flask import flask
 app= flask(__name__)
 @app.route("/")
 def home():
-    return "Hello World - Pushing code from PC to Axure Devops Repo"
+    return "Hello World - Pushing code from PC to Azure Devops Repo"
 
 
