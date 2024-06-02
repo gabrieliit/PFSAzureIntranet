e4e721d4e62aaaeb2725e568f21a82965623c271
gabrielthomas.v <gabrielthomas.v@gmail.com>
2024-06-02 18:25:09 +0530

    Updated UI framework with common page template, and extensible page content

--- Layouts/homepage.py
+++ Layouts/homepage.py
@@ -1,127 +1,109 @@
+#import python packages
 import dash_html_components as html
 import dash_core_components as dcc
 import dash
-from dash.dependencies import Input, Output
-import requests
-from azure.identity import ClientSecretCredential
-from azure.eventgrid import EventGridPublisherClient
-from azure.core.messaging import CloudEvent
+from dash import Input, Output,State,callback_context
+from datetime import datetime,timedelta
+import os
 import json
+import requests
 #import required project modules
+from Layouts import user_auth_flow as uf,styles
 #from Layouts import app_data as ad
-
-
-def draw_homepage(homepage_ext=[]):
-    layout=[            
-        dcc.Location(id='home_url', refresh=False),
-        html.H1('Dash App'),
-        dcc.Input(id='home_input_data', type='text', placeholder='Enter data'),
-        html.Button('SubmitREST', id='home_submit_button_REST', n_clicks=0),
-        html.Div(id='home_output_message'),
+event_count=0
+#Connection details
+hist_period=7
+end_date=datetime.today()
+start_date=end_date-timedelta(days=hist_period)
+end_date=end_date.strftime("%Y-%m-%d")
+start_date=start_date.strftime("%Y-%m-%d")
+METAL_PRICE_API_KEY=os.environ["METAL_PRICE_API_KEY"]
+METAL_PRICE_API_LATEST=f"https://api.metalpriceapi.com/v1/latest?api_key={METAL_PRICE_API_KEY}&base=XAU&currencies=INR"
+METAL_PRICE_API_HIST=f"https://api.metalpriceapi.com/v1/timeframe?api_key={METAL_PRICE_API_KEY}&start_date={start_date}&end_date={end_date}&base=XAU&currencies=INR"
+GM_PER_TROY_OUNCE=31.103
+#draw the homepage
+def draw_page_content(ext=[]):
+    #define hompage content
+    try:
+        with open("price.json", "r") as file:
+            last_price_point=json.load(file)
+            price=last_price_point["price"]
+            price="{:.2f}".format(float(price))#format as 2 decimal
+            source=last_price_point["source"]
+            date=last_price_point["date"]
+    except Exception as e:
+        price="NA"
+        source="NA"
+        date="NA"
+    homepage_content=html.Div([
         html.Div(
         [
-            
-            html.Label(event_count, id='home_event_count')
-        ]
+            html.Label("Last fetched gold price :", style={"margin-right":"10px"}),
+            dcc.Input(value=f"{price}",style={"margin-right":"10px"},id="home-tb-price-XAU-display",readOnly=True),
+            html.Label(f"Sourced from : {source} on {date}",id="home-lbl-price-XAU-source")
+        ]),         
+        html.Button("Fetch Latest Gold Price", id="home-btn-fetch-xau-latest"),
+        html.Div(id="home-div-price-XAU-display"),
+        dcc.ConfirmDialog(
+            id="home-confirm-dialog-xau-price-persist",
+            message="Do you want to save the data to a local file?"
         ),
-        html.Button('SubmitEvent', id='home_submit_button_Event', n_clicks=0),
-        html.Label(id="home_event_pub_status"),
-        html.Button('Get Last Post Request',id='home_btn_get_last_post_req'),
-        html.Label(id="home_lbl_last_post_request"),
-        html.Div(id="home_layout_extension",children=homepage_ext),
-        dcc.Link(id="home_link_logout",children="logout",href="/logout",)
-    ]
-    return layout
+        dcc.Store(id="home-store-fetched-xau-price-latest")
+    ], style=styles.CONTENT_STYLE)   
+    return homepage_content
 
-event_count=0
-page_layout=draw_homepage()
-#credentials
-subscription_id = 'your_subscription_id'
-resource_group_name = 'your_resource_group_name'
-topic_name = 'DashUI'
-
-#Define the callbacks
+#Define an register callbacks
 def register_callbacks(app):
     @app.callback(
-        Output('home_output_message', 'children'),
-        [Input('home_submit_button_REST', 'n_clicks')],
-        [dash.dependencies.State('home_input_data', 'value')],
+        Output("home-store-fetched-xau-price-latest", "data"),
+        Output("home-confirm-dialog-xau-price-persist", "displayed"),
+        Output("home-tb-price-XAU-display", "value"),
+        Output("home-lbl-price-XAU-source", "children"),
+        Input("home-btn-fetch-xau-latest", "n_clicks"),
+        Input("home-confirm-dialog-xau-price-persist", "submit_n_clicks"),
+        State("home-store-fetched-xau-price-latest", "data"),
         prevent_initial_call=True
     )
-    def make_upper_case(n_clicks, user_input):
-        if n_clicks > 0:
-            # Create event data with user input
-            try:
-                #response = send_data_to_azure_function(value)
-                response= invoke_REST_API_fn(user_input)
-            except Exception:
-                #msg ="Azure function call failed"
-                msg = "Failed to send event to Azure Event grid"
-            if response:
-                return "Uppercase Output: " + response.upper()
+    def fetch_and_confirm(n_clicks, submit_n_clicks,last_fetched_price):
+        ctx = callback_context
+        if not ctx.triggered:
+            raise dash.exceptions.PreventUpdate
+
+        triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+        if triggered_id == "home-btn-fetch-xau-latest":
+            # Replace with your API URL
+            url = METAL_PRICE_API_LATEST
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                price_per_gm=data["rates"]["INR"]/GM_PER_TROY_OUNCE
+                date=datetime.today().strftime("%Y-%m-%d")
+                price_point={"price":price_per_gm,"date":date,"source" : METAL_PRICE_API_LATEST,}
+                return price_point, True, "{:.2f}".format(float(price_per_gm)),f"Sourced from : {METAL_PRICE_API_LATEST} on {date}"
             else:
-                return msg
-        else:
-            return ""
+                try:
+                    with open("price.json", "r") as file:
+                        last_price_point=json.load(file)
+                        price=last_price_point["price"]
+                        source=last_price_point["source"]
+                        date=last_price_point["date"]
+                    return dash.no_update, dash.no_update, dash.no_update,f"Failed to fetch data. Last sourced from : {source} on {date}"
+                except:
+                    return dash.no_update, dash.no_update, dash.no_update,"Failed to fetch data"
+
+        elif triggered_id == "home-confirm-dialog-xau-price-persist":
+            if submit_n_clicks:
+                price=last_fetched_price["price"]
+                date=last_fetched_price["date"]
+                # Save data to local file
+                with open("price.json", "w") as f:
+                    json.dump(last_fetched_price, f)
+                return dash.no_update, False, "{:.2f}".format(float(price)), f"Sourced from {METAL_PRICE_API_LATEST} on {date}"
+            return dash.no_update, False, dash.no_update
+
+
         
-    @app.callback(
-        Output("home_lbl_last_post_request", 'children'),
-        [Input('home_btn_get_last_post_req', 'n_clicks')],
-        prevent_inital_call=True
-    )
-    def get_last_post_req(n_clicks):
-        if n_clicks is not None and n_clicks > 0:
-            # get last post reqeust
-            filename="app_data.txt"
-            # Read JSON data from a file and convert it into a dictionary
-            with open(filename, 'r') as file:
-                data = json.load(file)
-            return data.post_request
-        else:
-            return ""
-    @app.callback(
-        [Output('home_event_count', 'children'),Output('home_event_pub_status','children')],
-        [Input('home_submit_button_Event', 'n_clicks')],
-        [dash.dependencies.State('home_event_count', 'children')],
-        prevent_initial_call=True
-    )
-    def send_event_to_grid(nclicks,event_count):
-        pub_status=""
-        if nclicks>0:
-            event_count+=1
-            event_data={"event_count":event_count}
-            pub_status=pub_event("test_event",event_data)
-        return event_count,pub_status
-
-def pub_event(event_type,event_data):
-    topic_endpoint = "https://pfsintranetdashuicloudevents.southindia-1.eventgrid.azure.net/api/events"
-    client_id = "33f188e3-4576-4349-b367-08d3cd224619"
-    client_secret = "IN_8Q~OQ-tM9A7kLGRtbRh.RLwKDuPhEOPfJzaZX"
-    tenant_id = "f60dfc78-fca1-4c4c-9112-b231e45712f9"
-    # Authenticate using the service principal
-    credential = ClientSecretCredential(tenant_id, client_id, client_secret)
-    pub_client = EventGridPublisherClient(topic_endpoint, credential)
-    # Create a CloudEvent object with the event data
-    # Create a CloudEvent
-    event = CloudEvent\
-        (
-            type=event_type,
-            data=event_data,
-            source='http://127.0.0.1:8050/'
-         )
-    try:
-        # Publish the event to the Event Grid topic
-        response = pub_client.send(event)
-        return f'Event published successfully: {response}'
-    except Exception as e:
-        return f'Error publishing event: {str(e)}'
 
 
-def invoke_REST_API_fn(string):
-        function_url = 'https://pfsintranet-azfnapp.azurewebsites.net/api/http_trigger_sample2?'
-        payload = {'name': string}
-        response = requests.post(function_url, json=payload)
-        if response.status_code == 200:
-            return response.text
-        else:
-            return None
+
